widok/widok

commit 9b92feed121ccd0d0e051b15044b00a567a32664

Aggregate: Simplify FilterImpl()

src/main/scala/org/widok/Aggregate.scala
@@ -56,10 +56,7 @@ object Aggregate {
       map += ((parent, ch))
 
       ch.attach(value => {
-        parent.detach(observers(parent)) // TODO Find a better way.
-        parent.produce(value)
-        parent.attach(observers(parent))
-
+        parent.produce(value, observers(parent))
         if (!f(value)) delete(parent)
       })
 

commit fbc470fc42a32a21c0ab99349dac29c5f114bee9

Aggregate: Fix regression
Introduced by 9b92feed121ccd0d0e051b15044b00a567a32664

src/main/scala/org/widok/Aggregate.scala
@@ -56,7 +56,10 @@ object Aggregate {
       map += ((parent, ch))
 
       ch.attach(value => {
-        parent.produce(value, observers(parent))
+        parent.detach(observers(parent)) // TODO Find a better way.
+        parent.produce(value)
+        parent.attach(observers(parent))
+
         if (!f(value)) delete(parent)
       })
 

src/test/scala/org/widok/AggregateTest.scala
@@ -638,6 +638,7 @@ object AggregateTest extends JasmineTest {
       expect(size).toBe(1)
 
       val ch2 = agg2.append(1)
+      ch2 := 3
       expect(size).toBe(2)
 
       agg2.remove(ch2)