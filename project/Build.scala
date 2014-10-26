659c21117b14eee4c2938059a2c36a527f7e1948
Tim Nieradzik <tim@kognit.io>
2014-10-26 19:39:36 +0100

    Build: Inherit version from version.sbt

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,6 @@ import org.typelevel.sbt.TypelevelPlugin._
 
 object Build extends sbt.Build {
   val buildOrganisation = "org.widok"
-  val buildVersion = "0.1-SNAPSHOT"
   val buildScalaVersion = "2.11.2"
   val buildScalaOptions = Seq(
     "-unchecked", "-deprecation",
@@ -29,7 +28,6 @@ object Build extends sbt.Build {
         "name.myltsev" %% "shapeless_sjs0.5" % "2.0.0"
       ),
       organization := buildOrganisation,
-      version := buildVersion,
       scalaVersion := buildScalaVersion,
       scalacOptions := buildScalaOptions,
       ScalaJSKeys.persistLauncher := true
