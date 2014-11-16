2fc2fe706d888f57e64247ef04d2569df4cd78fc
Tim Nieradzik <tim@kognit.io>
2014-11-16 23:58:19 +0100

    Tree: Improve lookup methods

--- src/main/scala/org/widok/Tree.scala
+++ src/main/scala/org/widok/Tree.scala
@@ -4,19 +4,32 @@ trait Tree[T] {
   val value: Var[T]
   val children: VarBuf[Tree[T]]
 
-  def find(f: Var[Tree[T]] => Boolean): Option[Var[Tree[T]]] = {
-    val elem = children.elements.find(f)
-    if (elem.isDefined) elem
-    else children.elements.find(
-      _.get.find(f).isDefined
-    )
+  def find(f: Tree[T] => Boolean): Option[Tree[T]] =
+    if (f(this)) Some(this)
+    else children.elements.foldLeft(Option.empty[Tree[T]]) { (acc, cur) =>
+      if (acc.isDefined) acc
+      else cur.get.find(f)
+    }
+
+  def findChild(f: Var[Tree[T]] => Boolean): Option[Var[Tree[T]]] = {
+    val found = children.elements.find(f)
+    if (found.isDefined) found
+    else children.elements.foldLeft(Option.empty[Var[Tree[T]]]) { (acc, cur) =>
+      if (acc.isDefined) acc
+      else cur.get.findChild(f)
+    }
   }
 
-  def findChild(child: Var[Tree[T]]): Option[Tree[T]] = {
-    if (children.contains(child)) Some(this)
-    else children.elements.find(
-      _.get.findChild(child).isDefined
-    ).map(_.get)
+  def parentOf(node: Tree[T]): Option[Tree[T]] = {
+    val found = children.elements.find(
+      _.get.children.elements.exists(_.get == node)
+    )
+
+    if (found.isDefined) found.map(_.get)
+    else children.elements.foldLeft(Option.empty[Tree[T]]) { (acc, cur) =>
+      if (acc.isDefined) acc
+      else cur.get.parentOf(node)
+    }
   }
 }
 
@@ -24,6 +37,6 @@ object Tree {
   def apply[T](root: T, child: Tree[T]*): Tree[T] = new Tree[T] {
     val value = Var(root)
     val children = VarBuf(child: _*)
-    override def toString = "<tree>"
+    override def toString = s"Tree($value, $children)"
   }
 }
