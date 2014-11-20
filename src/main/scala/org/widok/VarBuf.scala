widok/widok

commit 68cdd840436f9b076740f1d4d6d7122a67a040ef

VarBuf: Fix filter()

src/main/scala/org/widok/VarBuf.scala
@@ -212,37 +212,33 @@ case class FilteredVarBuf[T](parent: ReadVarBuf[T], f: T => Boolean) extends Rea
       case Change.Insert(position, element) =>
         element.attach { value =>
           if (f(value)) {
-            chChanges := Change.Insert(mapPosition(position), element)
             mapping += element -> (())
+            chChanges := Change.Insert(mapPosition(position), element)
           } else if (mapping.isDefinedAt(element)) {
-            chChanges := Change.Remove(element)
             mapping -= element
+            chChanges := Change.Remove(element)
           }
         }
 
       case Change.Remove(element) =>
         // TODO detach above listener here
         if (mapping.isDefinedAt(element)) {
-          chChanges := Change.Remove(element)
           mapping -= element
+          chChanges := Change.Remove(element)
         }
 
       case Change.Clear() =>
-        clear()
+        // TODO detach above listener here
+        mapping.clear()
+        chChanges := Change.Clear()
     }
   }
 
   parent.changes.attach(parentChange)
 
-  private[widok] def clear() {
-    // TODO detach above listener here
-    mapping.clear()
-    chChanges := Change.Clear()
-  }
-
   /** Clear mapping and reapply f() for all parent elements. */
   private[widok] def reset() {
-    clear()
+    parentChange(Change.Clear())
     parent.changes.flush(parentChange)
   }
 
@@ -285,14 +281,15 @@ case class MappedVarBuf[T, U](parent: ReadVarBuf[T], f: T => U) extends ReadVarB
         chChanges := Change.Insert(position.map(mapping), mapping(element))
 
       case Change.Remove(element) =>
-        chChanges := Change.Remove(mapping(element))
+        val m = mapping(element)
         mapping(element).dispose()
         mapping -= element
+        chChanges := Change.Remove(m)
 
       case Change.Clear() =>
-        chChanges := Change.Clear()
         mapping.foreach(_._2.dispose())
         mapping.clear()
+        chChanges := Change.Clear()
     }
   }
 

src/test/scala/org/widok/VarBufTest.scala
@@ -16,4 +16,21 @@ object VarBufTest extends FunSuite {
     elem := 1
     Assert.isEquals(state, true)
   }
+
+  test("filter().lastOption") {
+    val buf = VarBuf[Int]()
+    val filter = buf.filter(_ > 1)
+    var last = -1
+
+    filter.lastOption.attach(cur ⇒ last = cur.get.get)
+
+    buf += 1
+    buf += 2
+    buf += 3
+
+    Assert.isEquals(last, 3)
+    buf.remove(buf(2))
+
+    Assert.isEquals(last, 2)
+  }
 }

commit 3ac40aea3854430f552f7106a48698a3f32626ee

VarBuf: Fix regression
Introduced in 68cdd840436f9b076740f1d4d6d7122a67a040ef

src/main/scala/org/widok/VarBuf.scala
@@ -197,12 +197,20 @@ case class FilteredVarBuf[T](parent: ReadVarBuf[T], f: T => Boolean) extends Rea
         if (mapping.isEmpty) Position.Last()
         else if (mapping.isDefinedAt(element)) position
         else {
-          val defined = parent.toSeq.filter(mapping.isDefinedAt)
-          val min = defined.minBy(cur => math.abs(parent.indexOf(cur) - parent.indexOf(element)))
+          val (leftSpan, rightSpan) = parent.toSeq.span(_ != element)
+          val left = leftSpan.reverse.find(mapping.isDefinedAt)
+          val right = rightSpan.find(mapping.isDefinedAt)
 
           position match {
-            case Position.Before(_) => Position.After(min)
-            case Position.After(_) => Position.Before(min)
+            case Position.Before(_) =>
+              left.map(Position.After(_))
+                .orElse(right.map(Position.Before(_)))
+                .get
+
+            case Position.After(_) =>
+              right.map(Position.Before(_))
+                .orElse(left.map(Position.After(_)))
+                .get
           }
         }
     }
@@ -212,8 +220,9 @@ case class FilteredVarBuf[T](parent: ReadVarBuf[T], f: T => Boolean) extends Rea
       case Change.Insert(position, element) =>
         element.attach { value =>
           if (f(value)) {
+            val pos = mapPosition(position)
             mapping += element -> (())
-            chChanges := Change.Insert(mapPosition(position), element)
+            chChanges := Change.Insert(pos, element)
           } else if (mapping.isDefinedAt(element)) {
             mapping -= element
             chChanges := Change.Remove(element)
@@ -275,10 +284,11 @@ case class MappedVarBuf[T, U](parent: ReadVarBuf[T], f: T => U) extends ReadVarB
   def parentChange(change: Change[Var[T]]) {
     change match {
       case Change.Insert(position, element) =>
+        val pos = position.map(mapping)
         /** TODO mapping += element -> element.map(f) */
         mapping += element -> Var(f(element.get))
         mapping(element) << element.map(f)
-        chChanges := Change.Insert(position.map(mapping), mapping(element))
+        chChanges := Change.Insert(pos, mapping(element))
 
       case Change.Remove(element) =>
         val m = mapping(element)