8cae842c3fa5f2d9100352d3d5a2696897d6a97d
Tim Nieradzik <tim@kognit.io>
2014-12-27 17:28:38 +0100

    Rework implicits

--- src/main/scala/org/widok.scala
+++ src/main/scala/org/widok.scala
@@ -24,32 +24,39 @@ package object widok {
     new Error().printStackTrace()
   }
 
-  implicit def WidgetToSeq[T <: Widget[T]](view: Widget[T]) = Seq(view)
+  implicit def StringToWidget(value: String): HTML.Text = HTML.Text(value)
 
-  implicit def StringToWidget(value: String) = HTML.Text(value)
+  implicit def StringChannelToWidget[T <: String](value: ReadChannel[T]):
+    HTML.Container.Inline = HTML.Container.Inline().bindString(value)
 
-  implicit def StringChannelToWidget[T <: String](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindString(value)
+  implicit def IntChannelToWidget[T <: Int](value: ReadChannel[T]):
+    HTML.Container.Inline = HTML.Container.Inline().bindInt(value)
 
-  implicit def IntChannelToWidget[T <: Int](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindInt(value)
+  implicit def DoubleChannelToWidget[T <: Double](value: ReadChannel[T]):
+    HTML.Container.Inline = HTML.Container.Inline().bindDouble(value)
 
-  implicit def DoubleChannelToWidget[T <: Double](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindDouble(value)
+  implicit def BooleanChannelToWidget[T <: Boolean](value: ReadChannel[T]):
+    HTML.Container.Inline = HTML.Container.Inline().bindBoolean(value)
 
-  implicit def BooleanChannelToWidget[T <: Boolean](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindBoolean(value)
+  implicit def WidgetChannelToWidget[T <: Widget[_]](value: ReadChannel[T]):
+    HTML.Container.Inline = HTML.Container.Inline().bindWidget(value)
 
-  implicit def WidgetChannelToWidget[T <: Widget[_]](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindWidget(value)
+  implicit def WidgetAggregateToWidget[T <: Widget[_]](agg: Aggregate[T]):
+    /* TODO Why do we have to cast? */
+    HTML.List.Items = HTML.List.Items(agg.asInstanceOf[Aggregate[Widget[_]]])
 
-  implicit def WidgetBufferToWidget[T <: Widget[_]](value: Aggregate[T]): HTML.List.Items =
-    HTML.List.Items(value.asInstanceOf[Aggregate[Widget[_]]]) // TODO Why do we have to cast?
+  implicit def StringBufferToWidget[T <: String](buf: ReadBuffer[T]):
+    HTML.List.Items =
+      /* TODO This could be optimised by creating fewer DOM nodes. */
+      HTML.List.Items(buf
+        .map(x => HTML.Text(x.get)))
 
-  implicit def OptWidgetChannelToWidget[T <: Option[Widget[_]]](value: ReadChannel[T]) =
-    HTML.Container.Inline().bindOptWidget(value)
+  implicit def OptWidgetChannelToWidget[T <: Option[Widget[_]]]
+    (value: ReadChannel[T]): HTML.Container.Inline =
+      HTML.Container.Inline().bindOptWidget(value)
 
-  implicit def InstantiatedRouteToString(route: InstantiatedRoute): String = route.uri()
+  implicit def InstantiatedRouteToString(route: InstantiatedRoute): String =
+    route.uri()
 
   implicit def FunctionToWriteChannel[T](f: T => Unit): WriteChannel[T] = {
     val ch = Channel[T]()
