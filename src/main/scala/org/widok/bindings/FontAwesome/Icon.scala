d7f13c0333321fe23c81c8572d95afc85968e665
Tim Nieradzik <tim.nieradzik@gmx.de>
2015-01-30 18:37:45 +0100

    FontAwesome: Reformat code

--- src/main/scala/org/widok/bindings/FontAwesome/Icon.scala
+++ src/main/scala/org/widok/bindings/FontAwesome/Icon.scala
@@ -13,48 +13,19 @@ trait Icon extends Widget[Icon] {
     css(s"fa-${level}x")
   }
 
-  def fixedWith(state: Boolean) = {
-    css(state, "fa-fw")
-  }
-
-  def unorderedList(state: Boolean) = {
-    css(state, "fa-ul")
-  }
-
-  def orderedList(state: Boolean) = {
-    css(state, "fa-li")
-  }
-
-  def border(state: Boolean) = {
-    css(state, "fa-border")
-  }
-
-  def spin(state: Boolean) = {
-    css(state, "fa-spin")
-  }
-
-  def pulse(state: Boolean) = {
-    css(state, "fa-pulse")
-  }
-
-  def rotate(deg: Int) = {
-    css(s"fa-rotate-$deg")
-  }
-
-  def flipHorizontal(state: Boolean) = {
-    css(state, "fa-flip-horizontal")
-  }
-
-  def flipVertical(state: Boolean) = {
-    css(state, "fa-flip-vertical")
-  }
-
   def stack(size: Int) = {
     assert((1 to 2).contains(size))
     css(s"fa-stack-${size}x")
   }
 
-  def inverse(state: Boolean) = {
-    css(state, "fa-inverse")
-  }
+  def fixedWith(state: Boolean) = css(state, "fa-fw")
+  def unorderedList(state: Boolean) = css(state, "fa-ul")
+  def orderedList(state: Boolean) = css(state, "fa-li")
+  def border(state: Boolean) = css(state, "fa-border")
+  def spin(state: Boolean) = css(state, "fa-spin")
+  def pulse(state: Boolean) = css(state, "fa-pulse")
+  def rotate(deg: Int) = css(s"fa-rotate-$deg")
+  def flipHorizontal(state: Boolean) = css(state, "fa-flip-horizontal")
+  def flipVertical(state: Boolean) = css(state, "fa-flip-vertical")
+  def inverse(state: Boolean) = css(state, "fa-inverse")
 }
