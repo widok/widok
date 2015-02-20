303875794c231a6ef9733a85f36f404bb48a3b81
Tim Nieradzik <tim@kognit.io>
2015-02-21 00:30:51 +0100

    Bootstrap: Move ModalBuilder to separate file

--- js/src/main/scala/org/widok/bindings/Bootstrap/package.scala
+++ js/src/main/scala/org/widok/bindings/Bootstrap/package.scala
@@ -486,45 +486,6 @@ package object Bootstrap {
     }
   }
 
-  case class ModalBuilder(contents: Modal.ContentElement*) extends Widget[ModalBuilder] {
-    val shown = Var(false)
-    val height = Var("")
-
-    def open() = { shown := true; this }
-    def dismiss() = { shown := false; this }
-
-    val rendered = Modal(
-      Modal.Backdrop().attribute("style", height)
-      , Modal.Dialog(
-        Modal.Content(contents: _*)
-      )
-    ).fade(true)
-      .cssState(shown, "in")
-      .rendered
-
-    /* .show(shown) wouldn't work here because Bootstrap uses
-     * `style.display = none` in its stylesheet.
-     */
-
-    val resize = (e: dom.Event) => {
-      val h = dom.document.body.scrollHeight
-      height := s"height: ${h}px"
-    }
-
-    shown.attach(
-      if (_) {
-        Document.body.className += "modal-open"
-        style.display := "block"
-        dom.window.addEventListener("resize", resize)
-        resize(null) /* Set initial height */
-      } else {
-        Document.body.className -= "modal-open"
-        style.display := "none"
-        dom.window.removeEventListener("resize", resize)
-      }
-    )
-  }
-
   case class Media(contents: View*) extends Widget[Media] {
     val rendered = DOM.createElement("div", contents)
     css("media")
