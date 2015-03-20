widok/widok

commit 970987786a9ee1c2b4d3fa59ddb87780b76d02c0

Widget: Add append() and remove()

js/src/main/scala/org/widok/Widget.scala
@@ -448,4 +448,17 @@ trait Widget[T] extends Node { self: T =>
       height = rendered.offsetHeight,
       top = rendered.offsetTop,
       left = rendered.offsetLeft)
+
+  /** Adds child widget */
+  def append(widget: Widget[_]) {
+    rendered.appendChild(widget.rendered)
+  }
+
+  /** Removes child widget */
+  def remove(widget: Widget[_]) {
+    rendered.removeChild(widget.rendered)
+  }
+
+  def +=(widget: Widget[_]) = append(widget)
+  def -=(widget: Widget[_]) = remove(widget)
 }

commit 8c416fed8362fea2e6f06817978b89408eb575d9

Fix JavaScript test cases
Regression introduced by 970987786a9ee1c2b4d3fa59ddb87780b76d02c0

js/src/main/scala/org/widok/Widget.scala
@@ -459,6 +459,9 @@ trait Widget[T] extends Node { self: T =>
     rendered.removeChild(widget.rendered)
   }
 
-  def +=(widget: Widget[_]) = append(widget)
-  def -=(widget: Widget[_]) = remove(widget)
+  // TODO Overloading the two operators leads to a regression in the JavaScript
+  // test cases.
+
+  // def +=(widget: Widget[_]) = append(widget)
+  // def -=(widget: Widget[_]) = remove(widget)
 }